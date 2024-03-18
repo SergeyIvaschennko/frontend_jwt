f685948609ef70d64e0599391ba7ebdea9fbb3da
SergeyIvaschennko <boss.awdr@mail.ru>
2024-03-18 10:48:50 +0300

    create login and register form login and logout

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -3,15 +3,75 @@ import * as React from 'react';
 import { request } from '../axios_helper';
 import WelcomeContent from "./WelcomeContent";
 import AuthContent from "./AuthContent";
+import LoginForm from "./LoginForm";
+import Buttons from "./Buttons";
 
 export default class AppContent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            componentToShow: "welcome"
+        }
+    };
+
+    login = () => {
+        this.setState({componentToShow: "login"})
+    };
+
+    logout = () => {
+        this.setState({componentToShow: "welcome"})
+    };
+
+    onLogin = (e, username, password) => {
+        e.preventDefault();
+        request(
+            "POST",
+            "/login",
+            {
+                login: username,
+                password: password
+            }).then(
+            (response) => {
+                this.setState({componentToShow: "messages"});
+            }).catch(
+            (error) => {
+                this.setState({componentToShow: "welcome"})
+            }
+        );
+    };
+
+    onRegister = (event, firstName, lastName, username, password) => {
+        event.preventDefault();
+        request(
+            "POST",
+            "/register",
+            {
+                firstName: firstName,
+                lastName: lastName,
+                login: username,
+                password: password
+            }).then(
+            (response) => {
+                this.setState({componentToShow: "messages"});
+            }).catch(
+            (error) => {
+                this.setState({componentToShow: "welcome"})
+            }
+        );
+    };
 
     render() {
         return (
-            <div>
-                <WelcomeContent/>
-                <AuthContent/>
-            </div>
+            <>
+                <Buttons
+                    login={this.login}
+                    logout={this.logout}
+                />
+
+                {this.state.componentToShow === "welcome" && <WelcomeContent /> }
+                {this.state.componentToShow === "login" && <LoginForm onLogin={this.onLogin} onRegister={this.onRegister} />}
+                {this.state.componentToShow === "messages" && <AuthContent />}
+            </>
         );
     };
 }
